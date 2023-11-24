9eb44479bee9cfed9fa13501bb9eb4e5665af85e
Paul Rivière <paulriviere002@gmail.com>
2023-11-24 15:59:23 +0100

    Oops

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -5,6 +5,7 @@ import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import {lusitana} from '@/app/ui/fonts';
 import {fetchCardData,} from '@/app/lib/data';
 import {Suspense} from 'react';
+
 import {
     RevenueChartSkeleton,
     LatestInvoicesSkeleton,
@@ -22,7 +23,14 @@ export default async function Page() {
                     <CardWrapper />
                 </Suspense>
             </div>
-            // ...
+            <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+                <Suspense fallback={<RevenueChartSkeleton/>}>
+                    <RevenueChart/>
+                </Suspense>
+                <Suspense fallback={<LatestInvoicesSkeleton/>}>
+                    <LatestInvoices/>
+                </Suspense>
+            </div>
         </main>
     );
 }
