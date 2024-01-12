d52a3ce02ccfff69c4004f1a5eed5b56e4b2ee29
Paul Rivière <paulriviere002@gmail.com>
2024-01-12 10:23:43 +0100

    Add credits in navbar #2

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -20,6 +20,7 @@ const links = [
     icon: DocumentDuplicateIcon,
   },
   { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  { name: 'Credits', href: '/dashboard/credits', icon: InboxIcon },
 ];
 
 export default function NavLinks() {
